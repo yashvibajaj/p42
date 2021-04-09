c2d24a117120fac87bde7d412c5581d185a33d80
yashvibajaj <76166448+yashvibajaj@users.noreply.github.com>
2021-04-09 11:00:12 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,19 @@
+//class for the man with an umbrella
+class Umbrella{
+    constructor(x, y){
+        var options = { 
+        'isStatic': true
+        }
+
+        this.body = Bodies.circle(x, y, 50, options);
+        this.radius = 50;
+        this.image = loadImage("walking_2.png");
+        World.add(world, this.body);
+    }
+ 
+      display(){
+        var pos = this.body.position;
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y+70,300,300);
+      }
+}
